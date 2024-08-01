f9f6c1611994f722ed65a302883286e856f6b099
Marcos Franco <marcosfranco.developer@gmail.com>
2024-08-01 14:48:01 -0300

    feat: configs
    Inclusão de configurações de meta e link no `index.html`
    Inclusão de environment no projeto
    Implementação de mapeamento de estilos do projeto
    Mapeamento de imports do módulo do projeto
    
    #001

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr, 'pt-BR');
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,10 +19,22 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
